5fc8494a56a2d2fd8537846a7fc51ce23c756acd
Marshall Johnson <marshalljohnson193@gmail.com>
2021-03-17 10:08:10 -0600

    some server stuff

---
+++ server/controllers/authController.js
@@ -0,0 +1,15 @@
+const bcrypt = require('bcryptjs')
+module.exports = {
+    register: (req,res) => {
+        
+    },
+    login: (req,res) => {
+
+    },
+    logout: (req,res) => {
+
+    },
+    getUser: (req,res) => {
+    
+    }
+}
--- server/index.js
+++ server/index.js
@@ -3,11 +3,16 @@ const express = require('express')
 const massive = require('massive')
 const session = require('express-session')
 const app = express()
+const authCtrl = require('./controllers/authController')
 
 
 const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
 app.use(express.json())
 
+app.post('/api/auth/register', authCtrl.register)
+app.post('/api/auth/login', authCtrl.login)
+app.delete('/api/auth/logout', auth.logout)
+app.get('/api/auth/getUser', authCtrl.getUser)
 
 massive({
     connectionString: CONNECTION_STRING,
@@ -21,3 +26,5 @@ massive({
         console.log(`Listening on ${SERVER_PORT}`)
     })
 })
+
+
