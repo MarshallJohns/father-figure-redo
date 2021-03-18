0eabb0d4cfa591d56a51bb71acac6e792407a201
Marshall Johnson <marshalljohnson193@gmail.com>
2021-03-18 12:21:50 -0600

    login and redux

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,10 +1,36 @@
+import axios from 'axios'
 import React, { useState } from 'react'
+import { loginUser } from '../../ducks/authReducer'
+import { connect } from 'react-redux'
 
-export default function Landing(props) {
-    const { email, setEmail } = useState('')
-    const { password, setPassword } = useState('')
+function Landing(props) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = () => {
+        if (email && password) {
+            axios.post('/api/auth/login', { email, password }).then(res => {
+                props.loginUser(res.data)
+                props.history.push('/home')
+            }).catch(err => {
+                alert(err.response.request.response)
+            })
+        } else (alert('Please fill out both fields'))
+    }
 
     return (
-        <div>Landing.js</div>
+        <div>
+            <div>
+                email:
+                <input value={email} type='text' onChange={e => setEmail(e.target.value)} />
+            </div>
+            <div>
+                password:
+                <input value={password} type='password' onChange={e => setPassword(e.target.value)} />
+            </div>
+            <button onClick={() => handleLogin()} >Submit</button>
+        </div>
     )
 }
+
+export default connect(null, { loginUser })(Landing)
---
+++ src/ducks/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import promiseMiddleware from 'redux-promise-middleware'
+import authReducer from './authReducer'
+
+export default createStore(
+    authReducer,
+    composeWithDevTools(applyMiddleware(promiseMiddleware))
+)
